9f63a728fd5ad65a078b4c311b58056a4278f438
xkhaliil <ltaief.khalil@gmail.com>
2023-05-18 23:24:00 +0100

    commit final

--- src/main/java/com/maher/section345/repos/LeagueRepos.java
+++
@@ -1,8 +0,0 @@
-package com.maher.section345.repos;
-
-import com.maher.section345.entities.League;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeagueRepos extends JpaRepository<League,Long> {
-
-}
--- src/main/java/com/maher/section345/security/SecParams.java
+++
@@ -1,7 +0,0 @@
-package com.maher.section345.security;
-
-public interface SecParams {
-    public static final long EXP_TIME = 10*24*60*60;
-    public static final String SECRET = "nadhemb@yahoo.com";
-    public static final String PREFIX = "Bearer ";
-}
